7fd1824d8014067bea49622c0d69815fd66cf818
izzysampsonn <97313340+izzysampsonn@users.noreply.github.com>
2024-01-17 17:12:14 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+namespace Mission2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Thread.Sleep(1000);
+            Console.WriteLine("How many dice rolls would you like to simulate?");
+            Console.Write("Enter number of rolls: ");
+            int numberOfRolls = int.Parse(Console.ReadLine());
+
+            RollDice rollDice = new RollDice();
+
+            int[] results = rollDice.SimulateDiceRolls(numberOfRolls);
+
+            Console.WriteLine("\nResults:");
+
+            for (int i = 0; i < results.Length; i++)
+            {
+                double percentage = (double)results[i] / numberOfRolls * 100;
+
+                Console.Write((i + 2) + ": ");
+
+                for (int j = 0; j < percentage; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine($"  ({percentage:F0}%)");
+            }
+            Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    public class RollDice
+    {
+        public int[] SimulateDiceRolls(int numberOfRolls)
+        {
+            int[] rollTotals = new int[11];
+            Random random = new Random();
+
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                int die1 = random.Next(1, 7);
+                int die2 = random.Next(1, 7);
+                int sum = die1 + die2;
+                rollTotals[sum - 2]++;
+
+            }
+            return rollTotals;
+        }
+    }
+}
+
+     
